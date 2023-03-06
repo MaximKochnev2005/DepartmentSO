f41c958443301e62a6962e8a428b7139ba7c167a
Maksim <maksim@Maksims-MacBook-Pro.local>
2023-03-06 17:53:30 +0300

    bot

---
+++ config/config.js
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv"
+dotenv.config()
+
+export const BOT_TOKEN = process.env.BOT_TOKEN;
+
+export const channels = {
+    'offer': -1001817620013,
+    'remark': -1001334063178,
+    'application': -1001877046012,
+    'review': -1001857630780
+}
